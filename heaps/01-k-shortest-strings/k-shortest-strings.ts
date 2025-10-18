44d43a8d0b6c2159138bff45e957e38f0015983b
Emeka Chukwulobe <emekabe@yahoo.com>
2025-10-18 13:38:16 +0100

    K shortest strings p definition

---
+++ heaps/01-k-shortest-strings/k-shortest-strings.ts
@@ -0,0 +1,8 @@
+function computeKShortestStrings(k: number, words: string[]): string[] {
+
+    return null;
+}
+
+
+
+console.log(computeKShortestStrings(3, ["apple", "ball", "cat", "dog", "elephant", "table", "cheese"]))
